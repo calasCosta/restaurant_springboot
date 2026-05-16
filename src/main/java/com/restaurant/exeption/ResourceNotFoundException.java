a9ceac749b784999bde204b4c230e46af7826d58
Noami Costa <65955770+calasCosta@users.noreply.github.com>
2026-05-16 04:57:19 +0100

    delete com.restaurant.exeption (it was a mistake)

--- src/main/java/com/restaurant/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.exeption;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
