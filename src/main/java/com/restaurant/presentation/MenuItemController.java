4fe1c50492c73a384064a9d4d85e6f914fa13282
Noami Costa <65955770+calasCosta@users.noreply.github.com>
2026-05-16 01:52:46 +0100

    Update MenuItemController.java to handle CRUD

--- src/main/java/com/restaurant/presentation/MenuItemController.java
+++ src/main/java/com/restaurant/presentation/MenuItemController.java
@@ -8,16 +8,28 @@ import java.util.concurrent.atomic.AtomicLong;
 record MenuItem(Long id, String name, double price){}
 
 @RestController
+@RequestMapping("/items")
 public class MenuItemController {
     private final List<MenuItem> menuItems = new ArrayList<>();
     private final AtomicLong idGenerator = new AtomicLong(1);
 
-    @GetMapping("/items")
+    //GET /items retrieve all
+    @GetMapping
     List<MenuItem> getAll(){
         return menuItems;
     }
 
-    @PostMapping("/items")
+    //GET /items/{id} - retrieve one
+    @GetMapping("/{id}")
+    MenuItem getOne(@PathVariable Long id){
+       return menuItems.stream()
+                .filter(item -> item.id().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Item not found"));
+    }
+
+    //POST /items - create new
+    @PostMapping
     MenuItem create(@RequestBody MenuItem newItem){
         MenuItem item = new MenuItem(
                 idGenerator.getAndIncrement(),
@@ -25,5 +37,23 @@ public class MenuItemController {
                 newItem.price());
         menuItems.add(item);
         return item;
+    }
+
+    //PUT /{id} - update existing
+    @PutMapping("/{id}")
+    MenuItem update(@PathVariable Long id, @RequestBody MenuItem updatedItem){
+        MenuItem existing = getOne(id); // reuse lookup
+        menuItems.remove(existing);
+
+        MenuItem newItem = new MenuItem(id, updatedItem.name(), updatedItem.price());
+        menuItems.add(newItem);
+        return newItem;
+    }
+
+    //DELETE /{id} - delete
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id){
+        MenuItem existing = getOne(id);
+        menuItems.remove(existing);
     }
 }
